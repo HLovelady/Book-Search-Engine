dbc6823642e31744ef17ad99f2fdee474ffcea9f
haylove <hfayelovelady@gmail.com>
2023-02-25 15:49:13 -0600

    apolloserver add authMiddleware

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const db = require('./config/connection');
 const routes = require('./routes');
 //require ApolloServer
 const { ApolloServer } = require('apollo-server-express');
-
+const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
@@ -12,6 +12,7 @@ const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: authMiddleware,
 });
 
 //app.use(express.urlencoded({ extended: true }));
@@ -23,7 +24,11 @@ app.use(express.json());
 //   app.use(express.static(path.join(__dirname, '../client/build')));
 // }
 
-// app.use(routes);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+app.use(routes);
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
