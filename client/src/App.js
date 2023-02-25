4874dc4c7b2792539d92cfc5e451f1e1679dcd9a
haylove <hfayelovelady@gmail.com>
2023-02-25 16:23:43 -0600

    authentication on app

--- client/src/App.js
+++ client/src/App.js
@@ -3,29 +3,42 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
-import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+import { ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client';
+// import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink} from '@apollo/client';
+// import { setContext } from '@apollo/client/link/context';
 
-const httpLink = createHttpLink({
-  uri: '/graphql',
-});
+// const httpLink = createHttpLink({
+//   uri: '/graphql',
+// });
 
 
-const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('id_token');
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    }
-  }
-});
+// const authLink = setContext((_, { headers }) => {
+//   // get the authentication token from local storage if it exists
+//   const token = localStorage.getItem('id_token');
+//   // return the headers to the context so httpLink can read them
+//   return {
+//     headers: {
+//       ...headers,
+//       authorization: token ? `Bearer ${token}` : '',
+//     }
+//   }
+// });
+
 
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache()
+  // link: authLink.concat(httpLink),
+  // cache: new InMemoryCache()
+  request: (operation) => {
+    const token = localStorage.getItem("id_token");
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    });
+  },
+  uri: "/graphql",
+  cache: new InMemoryCache(),
 });
 
 function App() {
--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
-import { useMutation } from '@apollo/client';
-import {  } from '../../utils/mutations';
-import {  } from '../../utils/queries';
+// import { useMutation } from '@apollo/client';
+// import {  } from '../../utils/mutations';
+// import {  } from '../../utils/queries';
 
 ReactDOM.render(
   <React.StrictMode>
